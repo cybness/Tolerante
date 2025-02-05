0b4682f4b19c05b7f8ae2650c63c3e41ec3cab48
Aranza <cybergoth2077@outlook.com>
2025-02-04 20:38:43 -0600

    First commit

---
+++ 27Jan2025/Try_catch/src/trycatch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner entero = new Scanner(System.in);
+        System.out.println("Introdue un número entero");
+        String numero = entero.nextLine();
+        try{
+            int resultado = Integer.parseInt(numero);
+            System.out.println("Felicidades, introduciste el número " + resultado);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Nope.");
+        } finally {
+            entero.close();
+        }
+            
+    }
+}
